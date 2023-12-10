67cf7a06fa7d3be1cfaf5e1846ea083206b8d49e
erihanapcschool <148433004+erihanapcschool@users.noreply.github.com>
2023-12-10 22:25:52 +0900

    scroll

--- script.js
+++ script.js
@@ -114,14 +114,18 @@ setInterval(() => {
 
 
 //ふよふよ
-document.querySelector(".square").animate({
-    borderRadius: [
+
+document.querySelector(".square").animate(
+    {
+      borderRadius: [
         "50% 50% 50% 70%/50% 50% 70% 60%",
         "80% 30% 50% 50%/50%",
         "40% 40% 50% 40%/30% 50% 40% 80%"
-    ]
-}, {
-    iterations: Infinity,
-    direction: "alternate",
-    duration: 7000
-});
+      ]
+    },
+    {
+      iterations: Infinity,
+      direction: "alternate",
+      duration: 7000
+    }
+  );
